a48c7bc51c1120ed9f92136d918abfd9fdbdea39
kiwi-oss <71145773+kiwi-oss@users.noreply.github.com>
2021-07-29 21:45:28 +0200

    Wrap script in main function
    This allows to declare functions below the places where they are first
    called. This way, the script can be read in a natural way from top to
    bottom with reduced visual jumping where one first reads what is done on
    a more abstract level and then goes on to read more details below.

--- validate_workload_declarations.sh
+++ validate_workload_declarations.sh
@@ -1,40 +1,46 @@
 #!/usr/bin/env sh
 
-docker run -it --rm  -v $(pwd):/workdir -w /workdir deck15/kubeval-tools /bin/sh -c  '
-CLUSTERS=`find ./clusters -type d -mindepth 2 -maxdepth 2 -not -name 'bases'`
-echo ${CLUSTERS}
+main() {
+    docker run -it --rm  -v $(pwd):/workdir -w /workdir deck15/kubeval-tools /bin/sh -c  '
+    CLUSTERS=`find ./clusters -type d -mindepth 2 -maxdepth 2 -not -name 'bases'`
+    echo ${CLUSTERS}
 
-for cluster in ${CLUSTERS}; do
-    echo
-    echo ==================== ;
-    echo check $cluster ;
-    echo ==================== ;
+    for cluster in ${CLUSTERS}; do
+        echo
+        echo ==================== ;
+        echo check $cluster ;
+        echo ==================== ;
 
-    echo
-    echo ... kustomize;
-    kustomize build ./${cluster} > generated_workloads ;
-    [ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: generating workloads! \e[0m" && exit 1;
-    echo -e "\e[32;1m✔️PASS\e[0m - workloads generated."
+        echo
+        echo ... kustomize;
+        kustomize build ./${cluster} > generated_workloads ;
+        [ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: generating workloads! \e[0m" && exit 1;
+        echo -e "\e[32;1m✔️PASS\e[0m - workloads generated."
 
-    echo
-    echo ... kubeval;
-    cat generated_workloads | kubeval --ignore-missing-schemas --exit-on-error ;
-    [ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: validating k8s schemas! \e[0m" && exit 1;
-    echo -e "\e[32;1m✔️PASS\e[0m - k8s schemas validated."
+        echo
+        echo ... kubeval;
+        cat generated_workloads | kubeval --ignore-missing-schemas --exit-on-error ;
+        [ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: validating k8s schemas! \e[0m" && exit 1;
+        echo -e "\e[32;1m✔️PASS\e[0m - k8s schemas validated."
 
-    echo
-    echo ... conftest;
-    cat generated_workloads | conftest test - ;
-    [ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: validating policies! \e[0m" && exit 1;
-    echo -e "\e[32;1m✔️PASS\e[0m - policies fullfileled."
+        echo
+        echo ... conftest;
+        cat generated_workloads | conftest test - ;
+        [ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: validating policies! \e[0m" && exit 1;
+        echo -e "\e[32;1m✔️PASS\e[0m - policies fullfileled."
 
-    echo
-    echo ... check unpatched values;
-    if [ $(cat generated_workloads | grep "<patched>" | wc -l) -ne 0 ]; then
-      echo -e "\e[31;1m❌ Error: found unpatched places! \e[0m";
-      exit 1;
-    fi
-    echo -e "\e[32;1m✔️PASS\e[0m - no unpatched places found."
+        echo
+        echo ... check unpatched values;
+        if [ $(cat generated_workloads | grep "<patched>" | wc -l) -ne 0 ]; then
+          echo -e "\e[31;1m❌ Error: found unpatched places! \e[0m";
+          exit 1;
+        fi
+        echo -e "\e[32;1m✔️PASS\e[0m - no unpatched places found."
 
-    done
-'
+        done
+    '
+}
+
+
+main "$@"
+
