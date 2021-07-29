2d62de9f6b4469343f18432fcadfb7e748dbb2a2
kiwi-oss <71145773+kiwi-oss@users.noreply.github.com>
2021-07-29 21:45:29 +0200

    Add mock for Find command
    Find interacts with the file system, so if we used it in the tests as
    is, we would have to create the necessary files and remove them for each
    test. We don't want to write such elaborated integration tests, but
    focus on rather simple unit tests here, so we mock Find.

--- spec/validate_workload_declarations_spec.sh
+++ spec/validate_workload_declarations_spec.sh
@@ -1,6 +1,10 @@
 Describe 'Workload declarations validation'
 
     It 'outputs success messages for all steps when they pass'
+        find() {
+            echo 'some_cluster'
+        }
+
         When run source validate_workload_declarations.sh
         The status should be success
         The output should include 'PASS'
