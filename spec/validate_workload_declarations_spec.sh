0513cb9ccc0def5f213bdd8945cba650090534be
kiwi-oss <71145773+kiwi-oss@users.noreply.github.com>
2021-07-29 21:45:29 +0200

    Indent with tabs instead of spaces
    This prepares for the next step where regular indentation with tabs is
    used to distinguish between indentation that should and the one that
    shouldn't be printed in a block.

--- spec/validate_workload_declarations_spec.sh
+++ spec/validate_workload_declarations_spec.sh
@@ -2,113 +2,113 @@
 
 Describe 'Workload declarations validation'
 
-    pass_message_count_is() {
-        local string=${pass_message_count_is:?}
-        local expected_count="$1"
+	pass_message_count_is() {
+		local text=${pass_message_count_is:?}
+		local expected_count="$1"
 
-        (( $(grep --ignore-case --count 'pass' <<< "$string") == expected_count ))
-    }
+		(( $(grep --ignore-case --count 'pass' <<< "$text") == expected_count ))
+	}
 
-    It 'outputs success messages for all steps when they pass'
-        find() {
-            printf '%s\n' 'some_cluster'
-        }
-        docker() {
-            printf '%s\n' 'Output from Kubeval tool'
-            return 0
-        }
-        cat() {
-            printf '%s\n' 'dummy workload'
-        }
+	It 'outputs success messages for all steps when they pass'
+		find() {
+			printf '%s\n' 'some_cluster'
+		}
+		docker() {
+			printf '%s\n' 'Output from Kubeval tool'
+			return 0
+		}
+		cat() {
+			printf '%s\n' 'dummy workload'
+		}
 
-        When run source validate_workload_declarations.sh
-        The status should be success
-        The output should satisfy pass_message_count_is 4
-    End
+		When run source validate_workload_declarations.sh
+		The status should be success
+		The output should satisfy pass_message_count_is 4
+	End
 
-    Describe 'with a failing Kubeval tool'
-        Parameters
-           'kustomize build ' 'generating workloads' 0
-           'kubeval --ignore-missing-schemas --exit-on-error' 'validating k8s schemas' 1
-           'conftest test' 'validating policies' 2
-        End
+	Describe 'with a failing Kubeval tool'
+		Parameters
+			'kustomize build ' 'generating workloads' 0
+			'kubeval --ignore-missing-schemas --exit-on-error' 'validating k8s schemas' 1
+			'conftest test' 'validating policies' 2
+		End
 
-        It 'exits with corresponding message'
-            validation_command="$1"
-            error_message="$2"
-            pass_message_count="$3"
+		It 'exits with corresponding message'
+			validation_command="$1"
+			error_message="$2"
+			pass_message_count="$3"
 
-            find() {
-                printf '%s\n' 'some_cluster'
-            }
-            docker() {
-                if [[ "$*" == *"$validation_command"* ]]; then
-                    return 1
-                fi
-                printf '%s\n' 'Output from Kubeval tool'
-                return 0
-            }
-            cat() {
-                printf '%s\n' 'dummy workload'
-            }
+			find() {
+				printf '%s\n' 'some_cluster'
+			}
+			docker() {
+				if [[ "$*" == *"$validation_command"* ]]; then
+					return 1
+				fi
+				printf '%s\n' 'Output from Kubeval tool'
+				return 0
+			}
+			cat() {
+				printf '%s\n' 'dummy workload'
+			}
 
-            When run source validate_workload_declarations.sh
-            The status should be failure
-            The output should include 'Error'
-            The output should include "$error_message"
-            The output should satisfy pass_message_count_is "$pass_message_count"
-        End
-    End
+			When run source validate_workload_declarations.sh
+			The status should be failure
+			The output should include 'Error'
+			The output should include "$error_message"
+			The output should satisfy pass_message_count_is "$pass_message_count"
+		End
+	End
 
-    It 'exits with corresponding message for failing patch validation'
-        find() {
-            printf '%s\n' 'some_cluster'
-        }
-        docker() {
-            printf '%s\n' 'Output from Kubeval tool'
-            return 0
-        }
-        cat() {
-            printf '%s\n' 'Unpatched workload <patched>'
-        }
+	It 'exits with corresponding message for failing patch validation'
+		find() {
+			printf '%s\n' 'some_cluster'
+		}
+		docker() {
+			printf '%s\n' 'Output from Kubeval tool'
+			return 0
+		}
+		cat() {
+			printf '%s\n' 'Unpatched workload <patched>'
+		}
 
-        When run source validate_workload_declarations.sh
-        The status should be failure
-        The output should include 'Error'
-        The output should include 'found unpatched places'
-        The output should satisfy pass_message_count_is 3
-    End
+		When run source validate_workload_declarations.sh
+		The status should be failure
+		The output should include 'Error'
+		The output should include 'found unpatched places'
+		The output should satisfy pass_message_count_is 3
+	End
 
-    It 'outputs success messages for multiple clusters'
-        find() {
-            printf '%s\n' 'first_cluster' 'second_cluster' 'third_cluster'
-        }
-        docker() {
-            printf '%s\n' 'Output from Kubeval tool'
-            return 0
-        }
-        cat() {
-            printf '%s\n' 'dummy workload'
-        }
+	It 'outputs success messages for multiple clusters'
+		find() {
+			printf '%s\n' 'first_cluster' 'second_cluster' 'third_cluster'
+		}
+		docker() {
+			printf '%s\n' 'Output from Kubeval tool'
+			return 0
+		}
+		cat() {
+			printf '%s\n' 'dummy workload'
+		}
 
-        When run source validate_workload_declarations.sh
-        The status should be success
-        The output should satisfy pass_message_count_is $(( 3 * 4 ))
-    End
+		When run source validate_workload_declarations.sh
+		The status should be success
+		The output should satisfy pass_message_count_is $(( 3 * 4 ))
+	End
 
-    It 'outputs the found cluster folders first'
-        find() {
-            printf '%s\n' 'first_cluster' 'second_cluster' 'third_cluster'
-        }
-        docker() {
-            return 1
-        }
+	It 'outputs the found cluster folders first'
+		find() {
+			printf '%s\n' 'first_cluster' 'second_cluster' 'third_cluster'
+		}
+		docker() {
+			return 1
+		}
 
-        When run source validate_workload_declarations.sh
-        The status should be failure
-        The line 1 should equal 'first_cluster'
-        The line 2 should equal 'second_cluster'
-        The line 3 should equal 'third_cluster'
-        The line 4 should equal ''
-    End
+		When run source validate_workload_declarations.sh
+		The status should be failure
+		The line 1 should equal 'first_cluster'
+		The line 2 should equal 'second_cluster'
+		The line 3 should equal 'third_cluster'
+		The line 4 should equal ''
+	End
 End
--- validate_workload_declarations.sh
+++ validate_workload_declarations.sh
@@ -1,56 +1,56 @@
 #!/usr/bin/env bash
 
 main() {
-    CLUSTERS=$(find ./clusters -mindepth 2 -maxdepth 2 -type d -not -name 'bases') \
-        || exit_with_message 'Error during search for cluster folders.'
-    echo "${CLUSTERS}"
+	CLUSTERS=$(find ./clusters -mindepth 2 -maxdepth 2 -type d -not -name 'bases') \
+		|| exit_with_message 'Error during search for cluster folders.'
+	echo "${CLUSTERS}"
 
-    for cluster in ${CLUSTERS}; do
-        echo
-        echo ==================== ;
-        echo check "$cluster" ;
-        echo ==================== ;
+	for cluster in ${CLUSTERS}; do
+		echo
+		echo ==================== ;
+		echo check "$cluster" ;
+		echo ==================== ;
 
-        echo
-        echo ... kustomize;
-        run_in_container "kustomize build ./${cluster} > generated_workloads ;"
-        [ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: generating workloads! \e[0m" && exit 1;
-        echo -e "\e[32;1m✔️PASS\e[0m - workloads generated."
+		echo
+		echo ... kustomize;
+		run_in_container "kustomize build ./${cluster} > generated_workloads ;"
+		[ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: generating workloads! \e[0m" && exit 1;
+		echo -e "\e[32;1m✔️PASS\e[0m - workloads generated."
 
-        echo
-        echo ... kubeval;
-        run_in_container "cat generated_workloads | kubeval --ignore-missing-schemas --exit-on-error ;"
-        [ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: validating k8s schemas! \e[0m" && exit 1;
-        echo -e "\e[32;1m✔️PASS\e[0m - k8s schemas validated."
+		echo
+		echo ... kubeval;
+		run_in_container "cat generated_workloads | kubeval --ignore-missing-schemas --exit-on-error ;"
+		[ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: validating k8s schemas! \e[0m" && exit 1;
+		echo -e "\e[32;1m✔️PASS\e[0m - k8s schemas validated."
 
-        echo
-        echo ... conftest;
-        run_in_container "cat generated_workloads | conftest test - ;"
-        [ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: validating policies! \e[0m" && exit 1;
-        echo -e "\e[32;1m✔️PASS\e[0m - policies fullfileled."
+		echo
+		echo ... conftest;
+		run_in_container "cat generated_workloads | conftest test - ;"
+		[ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: validating policies! \e[0m" && exit 1;
+		echo -e "\e[32;1m✔️PASS\e[0m - policies fullfileled."
 
-        echo
-        echo ... check unpatched values;
-        if [ $(cat generated_workloads | grep "<patched>" | wc -l) -ne 0 ]; then
-          echo -e "\e[31;1m❌ Error: found unpatched places! \e[0m";
-          exit 1;
-        fi
-        echo -e "\e[32;1m✔️PASS\e[0m - no unpatched places found."
-    done
+		echo
+		echo ... check unpatched values;
+		if [ $(cat generated_workloads | grep "<patched>" | wc -l) -ne 0 ]; then
+			echo -e "\e[31;1m❌ Error: found unpatched places! \e[0m";
+			exit 1;
+		fi
+		echo -e "\e[32;1m✔️PASS\e[0m - no unpatched places found."
+	done
 }
 
 exit_with_message() {
-    local exit_status="$?"
-    local message="$1"
+	local exit_status="$?"
+	local message="$1"
 
-    printf '%s\n' "$message" 'Aborting execution.'
-    exit "$exit_status"
+	printf '%s\n' "$message" 'Aborting execution.'
+	exit "$exit_status"
 }
 
 run_in_container() {
-    local command="$1"
+	local command="$1"
 
-    docker run -it --rm  -v "$(pwd)":/workdir -w /workdir deck15/kubeval-tools /bin/sh -c "$command"
+	docker run -it --rm  -v "$(pwd)":/workdir -w /workdir deck15/kubeval-tools /bin/sh -c "$command"
 }
 
 
