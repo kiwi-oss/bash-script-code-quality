db73bc75d7dea8d86ec35d640d82ee29192660af
kiwi-oss <71145773+kiwi-oss@users.noreply.github.com>
2021-07-29 21:45:29 +0200

    Remove redundant semicolons
    Semicolons are used in Bash to separate multiple commands in one line,
    so they are unnecessary at the end of lines. Also, it can be slightly
    confusing if they aren't added consistently.

--- validate_workload_declarations.sh
+++ validate_workload_declarations.sh
@@ -7,33 +7,33 @@ main() {
 
 	for cluster in ${CLUSTERS}; do
 		echo
-		echo ==================== ;
-		echo check "$cluster" ;
-		echo ==================== ;
+		echo ====================
+		echo check "$cluster"
+		echo ====================
 
 		echo
-		echo ... kustomize;
-		run_in_container "kustomize build ./${cluster} > generated_workloads ;"
-		[ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: generating workloads! \e[0m" && exit 1;
+		echo ... kustomize
+		run_in_container "kustomize build ./${cluster} > generated_workloads"
+		[ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: generating workloads! \e[0m" && exit 1
 		echo -e "\e[32;1m✔️PASS\e[0m - workloads generated."
 
 		echo
-		echo ... kubeval;
-		run_in_container "cat generated_workloads | kubeval --ignore-missing-schemas --exit-on-error ;"
-		[ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: validating k8s schemas! \e[0m" && exit 1;
+		echo ... kubeval
+		run_in_container "cat generated_workloads | kubeval --ignore-missing-schemas --exit-on-error"
+		[ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: validating k8s schemas! \e[0m" && exit 1
 		echo -e "\e[32;1m✔️PASS\e[0m - k8s schemas validated."
 
 		echo
-		echo ... conftest;
-		run_in_container "cat generated_workloads | conftest test - ;"
-		[ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: validating policies! \e[0m" && exit 1;
+		echo ... conftest
+		run_in_container "cat generated_workloads | conftest test -"
+		[ $? -ne 0 ] && echo -e "\e[31;1m❌ Error: validating policies! \e[0m" && exit 1
 		echo -e "\e[32;1m✔️PASS\e[0m - policies fullfileled."
 
 		echo
-		echo ... check unpatched values;
+		echo ... check unpatched values
 		if [ $(cat generated_workloads | grep "<patched>" | wc -l) -ne 0 ]; then
-			echo -e "\e[31;1m❌ Error: found unpatched places! \e[0m";
-			exit 1;
+			echo -e "\e[31;1m❌ Error: found unpatched places! \e[0m"
+			exit 1
 		fi
 		echo -e "\e[32;1m✔️PASS\e[0m - no unpatched places found."
 	done
