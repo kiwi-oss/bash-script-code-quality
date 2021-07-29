bec7e9b582b931c42e5946fed89618e4ffedc1d6
kiwi-oss <71145773+kiwi-oss@users.noreply.github.com>
2021-07-29 21:45:29 +0200

    Parameterise test for other Kubeval tools failing
    Failure of the other steps that are executed in the Docker container is
    tested by parameterising the validation command and the matching error
    message.

--- spec/validate_workload_declarations_spec.sh
+++ spec/validate_workload_declarations_spec.sh
@@ -26,24 +26,35 @@ Describe 'Workload declarations validation'
         The output should satisfy pass_message_count_is 4
     End
 
-    It 'exits with a failure messages if a step fails'
-        find() {
-            printf '%s\n' 'some_cluster'
-        }
-        docker() {
-            if [[ "$*" == *'kustomize build '* ]]; then
-                return 1
-            fi
-            printf '%s\n' 'Output from Kubeval tool'
-            return 0
-        }
-        cat() {
-            printf '%s\n' 'dummy workload'
-        }
+    Describe 'with a failing Kubeval tool'
+        Parameters
+           'kustomize build ' 'generating workloads'
+           'kubeval --ignore-missing-schemas --exit-on-error' 'validating k8s schemas'
+           'conftest test' 'validating policies'
+        End
 
-        When run source validate_workload_declarations.sh
-        The status should be failure
-        The output should include 'Error'
-        The output should include 'generating workloads'
+        It 'exits with corresponding message'
+            validation_command="$1"
+            error_message="$2"
+
+            find() {
+                printf '%s\n' 'some_cluster'
+            }
+            docker() {
+                if [[ "$*" == *"$validation_command"* ]]; then
+                    return 1
+                fi
+                printf '%s\n' 'Output from Kubeval tool'
+                return 0
+            }
+            cat() {
+                printf '%s\n' 'dummy workload'
+            }
+
+            When run source validate_workload_declarations.sh
+            The status should be failure
+            The output should include 'Error'
+            The output should include "$error_message"
+        End
     End
 End
